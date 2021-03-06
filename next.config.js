61f922d6626725dcc6d3b3459e63da8936bcbbd2
Santeri Elo <me@santeri.xyz>
2021-03-06 20:53:06 +0200

    feat(i18n): next-translate for i18n support

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
-module.exports = {
-  reactStrictMode: true,
-};
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const nextTranslate = require('next-translate');
+
+module.exports = nextTranslate();
