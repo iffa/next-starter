5a43bd628bf34646b35390a2edb1d6b4c945aca0
Santeri Elo <me@santeri.xyz>
2021-03-07 02:26:14 +0200

    ci: github action for cypress runs

---
+++ cypress/integration/index.ts
@@ -0,0 +1,29 @@
+describe('next-starter', () => {
+  it('successfully loads', () => {
+    cy.visit('/');
+  });
+
+  it('successfully loads alternative locale', () => {
+    cy.visit('/fi');
+  });
+
+  it('has correct text for default locale', () => {
+    cy.visit('/');
+
+    cy.get('h2').contains('Hello world.');
+  });
+
+  it('has correct text for alternative locale', () => {
+    cy.visit('/fi');
+
+    cy.get('h2').contains('Hei maailma.');
+  });
+
+  it('clicking language selector navigates to alternative locale', () => {
+    cy.visit('/');
+
+    cy.get('a').click();
+
+    cy.url().should('include', '/fi');
+  });
+});
