8c6a2ab3bc4ee3476887cbd67d30b53090de8668
Santeri Elo <me@santeri.xyz>
2021-03-08 20:25:13 +0200

    test: fix hello.spec.ts

--- cypress/integration/api/hello.spec.ts
+++ cypress/integration/api/hello.spec.ts
@@ -1,7 +1,17 @@
 describe('/api/hello', () => {
   it('returns correct data', () => {
-    cy.request('/api/hello').then((response) => {
+    cy.request('/api/hello?name=John Doe').then((response) => {
       expect(response.body).to.have.property('name', 'John Doe');
+    });
+  });
+
+  it('returns validation error for missing data', () => {
+    cy.request({
+      method: 'GET',
+      url: '/api/hello',
+      failOnStatusCode: false,
+    }).then((response) => {
+      expect(response.body).to.contain('name is a required field');
     });
   });
 });
