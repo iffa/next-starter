aff5cb522b93ffbb9178c8e61ee527192bf715ea
Santeri Elo <me@santeri.xyz>
2021-03-08 20:20:11 +0200

    feat: next-yup for api route schema validation

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,8 +1,22 @@
-import { NextApiRequest, NextApiResponse } from 'next';
+import withYup from 'next-yup';
+import * as yup from 'yup';
 
 /**
- * ! Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+ * Schema to validate the query/headers/body against.
+ * By default, next-yup attempts to type cast values to their
+ * proper types (e.g. string "boolean" to actual boolean value)
  */
-export default (req: NextApiRequest, res: NextApiResponse): void => {
-  return res.status(200).json({ name: 'John Doe' });
-};
+const helloSchema = yup.object().shape({
+  name: yup.string().required(),
+});
+
+/**
+ * Next.js API route documentation: https://nextjs.org/docs/api-routes/introduction
+ * next-yup for schema validation: https://github.com/iffa/next-yup
+ *
+ * Call this example API route with:
+ * GET http://localhost:3000/api/hello?name=John%20Doe
+ */
+export default withYup()({ query: helloSchema }, (req, res, data) => {
+  return res.status(200).json({ name: data.query['name'] });
+});
