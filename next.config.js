15a0b4ba3e40f03b172126027f1dcd814788a951
Santeri Elo <santeri.elo@cgi.com>
2021-04-09 00:37:29 +0300

    feat(i18n): use next-i18next for translations

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const nextTranslate = require('next-translate');
+const { i18n } = require('./next-i18next.config');
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
-module.exports = withBundleAnalyzer(nextTranslate());
+module.exports = withBundleAnalyzer({
+  i18n,
+});
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,30 @@
-import { getServerSideProps } from '@app/components/ChakraWrapper';
+import {
+  chakraWrapperProps,
+  ChakraWrapperPropsType,
+} from '@app/components/ChakraWrapper';
 import { Button, Heading, Stack, useColorMode } from '@chakra-ui/react';
-import useTranslation from 'next-translate/useTranslation';
+import { GetServerSideProps } from 'next';
+import { useTranslation } from 'next-i18next';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import React from 'react';
 
 export default function Home(): JSX.Element {
-  const { t, lang } = useTranslation();
+  const router = useRouter();
+  const { t } = useTranslation(['common', 'home']);
   const { toggleColorMode } = useColorMode();
+
   return (
     <Stack align="center" paddingY={8}>
       <Heading>{t('home:hello-world')}</Heading>
       <Button onClick={toggleColorMode}>{t('home:toggle-color-mode')}</Button>
-      {lang !== 'en' && (
+      {router.locale !== 'en' && (
         <Link href="/" locale="en">
           {t('common:english')}
         </Link>
       )}
-      {lang !== 'fi' && (
+      {router.locale !== 'fi' && (
         <Link href="/" locale="fi">
           {t('common:finnish')}
         </Link>
@@ -25,4 +33,13 @@ export default function Home(): JSX.Element {
   );
 }
 
-export { getServerSideProps };
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const chakra: ChakraWrapperPropsType = await chakraWrapperProps(context);
+
+  return {
+    props: {
+      ...chakra.props,
+      ...(await serverSideTranslations(context.locale, ['common', 'home'])),
+    },
+  };
+};
