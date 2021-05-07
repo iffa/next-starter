bbe5ecbec03d180f49abcedcab48fffe75f7e068
Santeri Elo <me@santeri.xyz>
2021-05-07 20:49:15 +0300

    fix(style): tailwind config cleanup and docs

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,20 +2,25 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
+  // If having trouble, disable JIT mode
+  // https://tailwindcss.com/docs/just-in-time-mode
+  mode: 'jit',
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       fontFamily: {
+        // Include default stack as fallback
         sans: ['Inter', ...defaultTheme.fontFamily.sans],
       },
     },
   },
   variants: {
     extend: {
-      opacity: ['disabled'],
-      cursor: ['disabled'],
-      textColor: ['visited'],
+      // Add new variants if needed
+      // opacity: ['disabled'],
+      // cursor: ['disabled'],
+      // textColor: ['visited'],
     },
   },
   plugins: [
